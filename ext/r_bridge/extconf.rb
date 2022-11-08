7e5559d611a66b50f0a111cadbcf461edb0ff384
niceume <toshi@niceume.com>
2022-11-08 17:36:54 +0900

    extconf.rb is improved for this gem to be compiled on more environments. r_bridge.rb is also improved to work on more environments.

--- ext/r_bridge/extconf.rb
+++ ext/r_bridge/extconf.rb
@@ -6,53 +6,69 @@ if FFI::Platform::OS == "windows"
   $CFLAGS << " " << "-D_WIN" << " "
 end
 
-
-class RHeaderNotFound < RuntimeError
-end
-class RLibraryNotFound < RuntimeError
+class RHeaderLibraryNotFound < RuntimeError
 end
 
-def check_R_header_and_lib( )
+def check_set_system_R_header_and_lib( )
   msg_devel_header = "For UNIX package users, please install R development tools via package (which name should look like r-base-dev or R-devel)."
   msg_devel_lib = "For UNIX package users, please install R development tools via package (which name should look like r-base-dev or R-devel). If this is a custom build of R, please make sure that It was built with the --enable-R-shlib option. "
 
   if have_header('R.h')
-    p "header ok"
+    p "header ok (system)"
     if have_library('R', 'R_tryEval') || have_library('libR', 'R_tryEval') 
-      p "library ok"
+      p "library ok (system)"
       return true
     else
-      raise RLibraryNotFound.new(  "Dynamic (i.e. shared) library of R is not found (R.dll for Windows, libR.so for UNIX)." + msg_devel_lib )
+      p "Dynamic (i.e. shared) library of R is not found (R.dll for Windows, libR.so for UNIX)." + msg_devel_lib
     end
   else
-    raise RHeaderNotFound.new( "Header for R is not found." + msg_devel_header )
+    p "Header for R is not found." + msg_devel_header
   end
+  return false
 end
 
-pkg_config_tried = false
-r_config_tried = false
+def check_set_pkg_config_R_header_and_lib( )
+  msg_pkg_config = "To let pkg-config locate libR.pc file, include its existing directory path in R PKG_CONFIG_PATH"
 
-dir_config("R")
-
-begin
-  if check_R_header_and_lib()
-    # Makefile that will build and install extension to lib/r_bridge/librbridge.so
-    create_makefile "r_bridge/librbridge"
-  end
-rescue RHeaderNotFound, RLibraryNotFound => e
-  if find_executable('pkg-config') && (pkg_config_tried == false)
+  if find_executable('pkg-config')
     add_cflags = pkg_config("libR", "cflags")
     add_ldflags = pkg_config("libR", "libs")
-    if ( ! add_cflags.nil? ) && (! add_ldflags.nil?)
+    if ( ! add_cflags.nil? )
+      p "header ok (pkg-config)"
       $CFLAGS << " " << add_cflags
-      $LDFLAGS <<" " << add_ldflags
+      if (! add_ldflags.nil?)
+        p "library ok (pkg-config)"
+        $LDFLAGS <<" " << add_ldflags
+        return true
+      else
+        p "Dynamic (i.e. shared) library of R is not found (R.dll for Windows, libR.so for UNIX)." + msg_pkg_config
+      end
+    else
+      p "Header for R is not found." + msg_pkg_config
     end
-    pkg_config_tried = true
-    retry
-  elsif ! find_executable('R')
-    raise "R program is not found. Please check your PATH setting if you already have R on your machine."
-  elsif find_executable('R') && find_executable('Rscript') && (r_config_tried == false)
-    r_home_path = `Rscript -e "cat(R.home()[1])"`.chomp
+  else
+    p "pkg-config command is not available."
+  end
+  return false
+end
+
+def check_Rscript_executable
+  if find_executable('Rscript')
+    return true
+  else
+    p "R program is not found. Please check your PATH setting if you already have R on your machine."
+  end
+  return false
+end
+
+def check_set_R_home_header_and_lib
+    msg_rscript = "Cannot be detected by R home."
+
+    if check_Rscript_executable
+      r_home_path = `Rscript -e "cat(R.home()[1])"`.chomp
+    else
+      return false
+    end
     
     if ! r_home_path.empty?()
       case FFI::Platform::ARCH
@@ -65,14 +81,32 @@ rescue RHeaderNotFound, RLibraryNotFound => e
       else
         raise FFI::Platform::ARCH + ": unkown architecure detected. Please specify R shared library by yourself."
       end
-      find_header('R.h', *possible_r_header_dirs)
-      find_library('R', 'R_tryEval', *possible_r_lib_dirs)
-    end
 
-    r_config_tried = true
-    retry
-  else
-    raise e
-  end
+      if find_header('R.h', *possible_r_header_dirs)
+        p "header ok (Rscript)"
+        if find_library('R', 'R_tryEval', *possible_r_lib_dirs)
+          p "library ok (Rscript)"
+          return true
+        else
+          p "Dynamic (i.e. shared) library of R is not found (R.dll for Windows, libR.so for UNIX)." + msg_rscript
+        end
+      else
+        p "Header for R is not found." + msg_rscript
+      end
+    else
+      p "R home cannot be detected."
+    end
+    return false
 end
+
+dir_config("R")
+
+if check_set_system_R_header_and_lib()
+elsif check_set_pkg_config_R_header_and_lib()
+elsif check_set_R_home_header_and_lib()
+else
+  raise RHeaderLibraryNotFound.new( "R header or library not fouund.")
+end
+
+create_makefile "r_bridge/librbridge"
 
--- lib/r_bridge.rb
+++ lib/r_bridge.rb
@@ -6,5 +6,21 @@ require 'r_bridge/r_bridge_lazyfunc_ext'
 module RBridge
   class Error < StandardError; end
   # Your code goes here...
+
+  if ENV["R_HOME"].nil?
+    puts "Environment variable R_HOME is not set." 
+    puts "This time, it is tried to be set by 'R RHOME' command."
+    begin
+      `R --version`
+    rescue => e
+      puts "R command is not available. Please ensure that your PATH environment variable includes the location of R command."
+      raise e
+    end
+
+    r_home_output = `R RHOME`
+    r_home = r_home_output.chomp
+    ENV["R_HOME"] = r_home
+    puts "R_HOME environment variable is set to be #{r_home}."
+  end
 end
 
